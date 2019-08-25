f46817c060f31ba10cf7f0b548030b852a9f3003
rassy yassine <yassine.rassy1@gmail.com>
2019-08-25 20:39:35 +0200

    add the some style to the storyDetails page and use a absolute path to access to it

--- src/app/mocks/stories.mock.ts
+++ src/app/mocks/stories.mock.ts
@@ -1,42 +1,120 @@
     export const STORIES = [
-    {
-        id: "1",
-        title: "The Greedy Mouse",
-        content: `A greedy mouse saw a basket full of corn. He wanted to eat it. So he made a small hole in the basket. He squeezed in through the hole. He ate a lot of corn. He felt full and was very happy.
-        Now he wanted to come out. He tried to come out through the small hole. He could not. His belly was full. H e tried again. But it was of no use. 
-        The mouse started crying. A rabbit was passing by. It heard the mouse’s cry and asked, “Why are you crying, my friend?”s
-        The mouse explained, “I made a small hole and came into the basket to eat the corn. Now I am not able to get out throughs that hole.”
-        The rabbit said, “It is because you ate too much. Wait till your belly shrinks.” The rabbit laughed and went away.ss
-        The mouse fell asleep in the basket. The next morning his belly had shrunk. But he wanted to eat some more corn. He forgot all about getting out of the basket. So he ate the corn and his belly was really big again.s
-        After eating, the mouse remembered that he had to escape. But obviously, he could not. So he thought, “Oh! Now I will go out tomorrow.”s
-        The cat was the next passerby. He smelt the mouse in the basket. He lifted its lid and ate the mouse.`,
-        imgURL:"https://i.ytimg.com/vi/K6X8HME8GtQ/maxresdefault.jpg"
-    }, {
-        id: "2",
-        title: "The Lion and the Mouse",
-        content: `Once when a lion, the king of the jungle, was asleep, a little mouse began running up and down on him. This soon awakened the lion, who placed his huge paw on the mouse, and opened his big jaws to swallow him.
-
-        "Pardon, O King!" cried the little mouse. "Forgive me this time. I shall never repeat it and I shall never forget your kindness. And who knows, I may be able to do you a good turn one of these days!”
-        
-        The lion was so tickled by the idea of the mouse being able to help him that he lifted his paw and let him go.
-        
-        Sometime later, a few hunters captured the lion, and tied him to a tree. After that they went in search of a wagon, to take him to the zoo.Just then the little mouse happened to pass by. On seeing the lion’s plight, he ran up to him and gnawed away the ropes that bound him, the king of the jungle.
-
-        "Was I not right?" said the little mouse, very happy to help the lion.
-        
-        MORAL: Small acts of kindness will be rewarded greatly.`,
-
-        imgURL: "https://www.kidsworldfun.com/images/short-stories/short_stories_lionandmouse3." +
-                "jpg"
-    }, {
-        id: "3",
-        title: "The Goose with the Golden Eggs",
-        content: `Once upon a time, a man and his wife had the good fortune to have a goose which laid a golden egg every day. Lucky though they were, they soon began to think they were not getting rich fast enough.<br/>
-
-        They imagined that if the bird is able to lay golden eggs, its insides must be made of gold. And they thought that if they could get all that precious metal at once, they would get mighty rich very soon. So the man and his wife decided to kill the bird.<br/>
-        
-        However, upon cutting the goose open, they were shocked to find that its innards were like that of any other goose!`,
-        imgURL: "https://www.kidsworldfun.com/images/short-stories/short_stories_goosewithgolde" +
-                "neggs2.jpg"
-    }
+        {
+            id: "1",
+            title: "The Greedy Mouse",
+            content: `A greedy mouse saw a basket full of corn. He wanted to eat it. So he made a small hole in the basket. He squeezed in through the hole. He ate a lot of corn. He felt full and was very happy.
+            Now he wanted to come out. He tried to come out through the small hole. He could not. His belly was full. H e tried again. But it was of no use. 
+            The mouse started crying. A rabbit was passing by. It heard the mouse’s cry and asked, “Why are you crying, my friend?”s
+            The mouse explained, “I made a small hole and came into the basket to eat the corn. Now I am not able to get out throughs that hole.”
+            The rabbit said, “It is because you ate too much. Wait till your belly shrinks.” The rabbit laughed and went away.ss
+            The mouse fell asleep in the basket. The next morning his belly had shrunk. But he wanted to eat some more corn. He forgot all about getting out of the basket. So he ate the corn and his belly was really big again.s
+            After eating, the mouse remembered that he had to escape. But obviously, he could not. So he thought, “Oh! Now I will go out tomorrow.”s
+            The cat was the next passerby. He smelt the mouse in the basket. He lifted its lid and ate the mouse.`,
+            imgURL:"https://i.ytimg.com/vi/K6X8HME8GtQ/maxresdefault.jpg"
+        }, {
+            id: "2",
+            title: "The Lion and the Mouse",
+            content: `Once when a lion, the king of the jungle, was asleep, a little mouse began running up and down on him. This soon awakened the lion, who placed his huge paw on the mouse, and opened his big jaws to swallow him.
+    
+            "Pardon, O King!" cried the little mouse. "Forgive me this time. I shall never repeat it and I shall never forget your kindness. And who knows, I may be able to do you a good turn one of these days!”
+            
+            The lion was so tickled by the idea of the mouse being able to help him that he lifted his paw and let him go.
+            
+            Sometime later, a few hunters captured the lion, and tied him to a tree. After that they went in search of a wagon, to take him to the zoo.Just then the little mouse happened to pass by. On seeing the lion’s plight, he ran up to him and gnawed away the ropes that bound him, the king of the jungle.
+    
+            "Was I not right?" said the little mouse, very happy to help the lion.
+            
+            MORAL: Small acts of kindness will be rewarded greatly.`,
+    
+            imgURL: "https://www.kidsworldfun.com/images/short-stories/short_stories_lionandmouse3." +
+                    "jpg"
+        }, {
+            id: "3",
+            title: "The Goose with the Golden Eggs",
+            content: `Once upon a time, a man and his wife had the good fortune to have a goose which laid a golden egg every day. Lucky though they were, they soon began to think they were not getting rich fast enough.<br/>
+    
+            They imagined that if the bird is able to lay golden eggs, its insides must be made of gold. And they thought that if they could get all that precious metal at once, they would get mighty rich very soon. So the man and his wife decided to kill the bird.<br/>
+            
+            However, upon cutting the goose open, they were shocked to find that its innards were like that of any other goose!`,
+            imgURL: "https://www.kidsworldfun.com/images/short-stories/short_stories_goosewithgolde" +
+                    "neggs2.jpg"
+        },{
+            id: "1",
+            title: "The Greedy Mouse",
+            content: `A greedy mouse saw a basket full of corn. He wanted to eat it. So he made a small hole in the basket. He squeezed in through the hole. He ate a lot of corn. He felt full and was very happy.
+            Now he wanted to come out. He tried to come out through the small hole. He could not. His belly was full. H e tried again. But it was of no use. 
+            The mouse started crying. A rabbit was passing by. It heard the mouse’s cry and asked, “Why are you crying, my friend?”s
+            The mouse explained, “I made a small hole and came into the basket to eat the corn. Now I am not able to get out throughs that hole.”
+            The rabbit said, “It is because you ate too much. Wait till your belly shrinks.” The rabbit laughed and went away.ss
+            The mouse fell asleep in the basket. The next morning his belly had shrunk. But he wanted to eat some more corn. He forgot all about getting out of the basket. So he ate the corn and his belly was really big again.s
+            After eating, the mouse remembered that he had to escape. But obviously, he could not. So he thought, “Oh! Now I will go out tomorrow.”s
+            The cat was the next passerby. He smelt the mouse in the basket. He lifted its lid and ate the mouse.`,
+            imgURL:"https://i.ytimg.com/vi/K6X8HME8GtQ/maxresdefault.jpg"
+        }, {
+            id: "2",
+            title: "The Lion and the Mouse",
+            content: `Once when a lion, the king of the jungle, was asleep, a little mouse began running up and down on him. This soon awakened the lion, who placed his huge paw on the mouse, and opened his big jaws to swallow him.
+    
+            "Pardon, O King!" cried the little mouse. "Forgive me this time. I shall never repeat it and I shall never forget your kindness. And who knows, I may be able to do you a good turn one of these days!”
+            
+            The lion was so tickled by the idea of the mouse being able to help him that he lifted his paw and let him go.
+            
+            Sometime later, a few hunters captured the lion, and tied him to a tree. After that they went in search of a wagon, to take him to the zoo.Just then the little mouse happened to pass by. On seeing the lion’s plight, he ran up to him and gnawed away the ropes that bound him, the king of the jungle.
+    
+            "Was I not right?" said the little mouse, very happy to help the lion.
+            
+            MORAL: Small acts of kindness will be rewarded greatly.`,
+    
+            imgURL: "https://www.kidsworldfun.com/images/short-stories/short_stories_lionandmouse3." +
+                    "jpg"
+        }, {
+            id: "3",
+            title: "The Goose with the Golden Eggs",
+            content: `Once upon a time, a man and his wife had the good fortune to have a goose which laid a golden egg every day. Lucky though they were, they soon began to think they were not getting rich fast enough.<br/>
+    
+            They imagined that if the bird is able to lay golden eggs, its insides must be made of gold. And they thought that if they could get all that precious metal at once, they would get mighty rich very soon. So the man and his wife decided to kill the bird.<br/>
+            
+            However, upon cutting the goose open, they were shocked to find that its innards were like that of any other goose!`,
+            imgURL: "https://www.kidsworldfun.com/images/short-stories/short_stories_goosewithgolde" +
+                    "neggs2.jpg"
+        },{
+            id: "1",
+            title: "The Greedy Mouse",
+            content: `A greedy mouse saw a basket full of corn. He wanted to eat it. So he made a small hole in the basket. He squeezed in through the hole. He ate a lot of corn. He felt full and was very happy.
+            Now he wanted to come out. He tried to come out through the small hole. He could not. His belly was full. H e tried again. But it was of no use. 
+            The mouse started crying. A rabbit was passing by. It heard the mouse’s cry and asked, “Why are you crying, my friend?”s
+            The mouse explained, “I made a small hole and came into the basket to eat the corn. Now I am not able to get out throughs that hole.”
+            The rabbit said, “It is because you ate too much. Wait till your belly shrinks.” The rabbit laughed and went away.ss
+            The mouse fell asleep in the basket. The next morning his belly had shrunk. But he wanted to eat some more corn. He forgot all about getting out of the basket. So he ate the corn and his belly was really big again.s
+            After eating, the mouse remembered that he had to escape. But obviously, he could not. So he thought, “Oh! Now I will go out tomorrow.”s
+            The cat was the next passerby. He smelt the mouse in the basket. He lifted its lid and ate the mouse.`,
+            imgURL:"https://i.ytimg.com/vi/K6X8HME8GtQ/maxresdefault.jpg"
+        }, {
+            id: "2",
+            title: "The Lion and the Mouse",
+            content: `Once when a lion, the king of the jungle, was asleep, a little mouse began running up and down on him. This soon awakened the lion, who placed his huge paw on the mouse, and opened his big jaws to swallow him.
+    
+            "Pardon, O King!" cried the little mouse. "Forgive me this time. I shall never repeat it and I shall never forget your kindness. And who knows, I may be able to do you a good turn one of these days!”
+            
+            The lion was so tickled by the idea of the mouse being able to help him that he lifted his paw and let him go.
+            
+            Sometime later, a few hunters captured the lion, and tied him to a tree. After that they went in search of a wagon, to take him to the zoo.Just then the little mouse happened to pass by. On seeing the lion’s plight, he ran up to him and gnawed away the ropes that bound him, the king of the jungle.
+    
+            "Was I not right?" said the little mouse, very happy to help the lion.
+            
+            MORAL: Small acts of kindness will be rewarded greatly.`,
+    
+            imgURL: "https://www.kidsworldfun.com/images/short-stories/short_stories_lionandmouse3." +
+                    "jpg"
+        }, {
+            id: "3",
+            title: "The Goose with the Golden Eggs",
+            content: `Once upon a time, a man and his wife had the good fortune to have a goose which laid a golden egg every day. Lucky though they were, they soon began to think they were not getting rich fast enough.<br/>
+    
+            They imagined that if the bird is able to lay golden eggs, its insides must be made of gold. And they thought that if they could get all that precious metal at once, they would get mighty rich very soon. So the man and his wife decided to kill the bird.<br/>
+            
+            However, upon cutting the goose open, they were shocked to find that its innards were like that of any other goose!`,
+            imgURL: "https://www.kidsworldfun.com/images/short-stories/short_stories_goosewithgolde" +
+                    "neggs2.jpg"
+        }
 ]
